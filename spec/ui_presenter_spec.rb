d469bdcffde62c710a879b783d1f11a1ae95bd4b
getsometoast <hijameslewis@gmail.com>
2014-11-27 17:53:28 +1100

    add failing test to check ui_presenter reads user input

--- spec/ui_presenter_spec.rb
+++ spec/ui_presenter_spec.rb
@@ -5,12 +5,25 @@ describe UIPresenter do
   describe '#run' do
 
     it 'displays options to the user' do
-
       fake_stdout = double('$stdout')
       fake_stdin = double('$stdin')
 
       expect(fake_stdout).to receive(:puts).with('Enter EXIT to exit at anytime')
       expect(fake_stdout).to receive(:puts).with('Enter START to start the simulator')
+      allow(fake_stdin).to receive(:chomp)
+
+      ui_presenter = UIPresenter.new stdout: fake_stdout,
+                                     stdin: fake_stdin
+
+      ui_presenter.run
+    end
+
+    it 'reads initial command from the user' do
+      fake_stdout = double('$stdout')
+      fake_stdin = double('$stdin')
+
+      allow(fake_stdout).to receive(:puts)
+      expect(fake_stdin).to receive(:chomp)
 
       ui_presenter = UIPresenter.new stdout: fake_stdout,
                                      stdin: fake_stdin

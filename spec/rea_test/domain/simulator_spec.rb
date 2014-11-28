f3b4e41d0c0194330c09c78a5c93e05a46d29ece
getsometoast <hijameslewis@gmail.com>
2014-11-28 20:01:15 +1100

    update simulator tests to reflect new design

--- spec/rea_test/domain/simulator_spec.rb
+++ spec/rea_test/domain/simulator_spec.rb
@@ -2,28 +2,30 @@ require_relative '../../support/spec_helper'
 
 describe ReaTest::Domain::Simulator do
 
-  describe '#execute' do
+  describe '#place' do
 
-    it 'executes a place command' do
+    it 'places the robot on the table' do
       robot = double('robot')
       position = ReaTest::Domain::Position.new(0, 0, :north)
-      command = ReaTest::Domain::Command.new type: :place,
-                                             parameters: { position: position }
 
       expect(robot).to receive(:place).with(position)
 
       simulator = ReaTest::Domain::Simulator.new robot: robot
-      simulator.execute(command)
+      simulator.place(position)
     end
 
-    it 'executes a report command' do
-      robot = double('robot')
-      command = ReaTest::Domain::Command.new type: :report
+  end
 
-      expect(robot).to receive(:report)
+  describe '#report' do
+
+    it 'reports the position of the robot' do
+      robot = double('robot')
+      position = ReaTest::Domain::Position.new(1, 1, :north)
+
+      expect(robot).to receive(:report) { position }
 
       simulator = ReaTest::Domain::Simulator.new robot: robot
-      simulator.execute(command)
+      expect(simulator.report).to eq(position.to_s)
     end
 
   end

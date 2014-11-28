6ac1a8fa9214a0af84c7f76f7fae0754476751ff
getsometoast <hijameslewis@gmail.com>
2014-11-28 16:34:46 +1100

    add failing test to check parser can parse report command

---
+++ spec/rea_test/user_interface/parser_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../../support/spec_helper'
+
+describe ReaTest::UserInterface::Parser do
+
+  describe '#parse' do
+
+    it 'parses a report command' do
+      parser = Parser.new
+      command = parser.parse('REPORT')
+
+      expect(command).to be_a(ReaTest::Domain::Command)
+      expect(command.type).to eq(:report)
+      expect(command.parameters).to be_nil
+    end
+
+  end
+
+end

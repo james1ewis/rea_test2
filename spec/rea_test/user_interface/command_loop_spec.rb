ed4438fca9cd4f0810bae45b003194efd9da909d
getsometoast <hijameslewis@gmail.com>
2014-11-28 22:19:52 +1100

    introduce new colaborator in command_loop

--- spec/rea_test/user_interface/command_loop_spec.rb
+++ spec/rea_test/user_interface/command_loop_spec.rb
@@ -65,6 +65,23 @@ describe ReaTest::UserInterface::CommandLoop do
       command_loop.start
     end
 
+    it 'executes commands' do
+      fake_stdout = double('$stdout')
+      fake_stdin = double('$stdin')
+      command_executor = double('CommandExecutor')
+
+      expect(fake_stdout).to receive(:puts).twice.with('Enter Command: ')
+      expect(fake_stdin).to receive(:gets) { "MOVE\n" }
+      expect(command_executor).to receive(:execute).with('MOVE')
+      expect(fake_stdin).to receive(:gets) { "EXIT\n" }
+
+      command_loop = ReaTest::UserInterface::CommandLoop.new stdout: fake_stdout,
+                                                             stdin: fake_stdin,
+                                                             command_executor: command_executor
+
+      command_loop.start
+    end
+
   end
 
 end

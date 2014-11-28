ec485ff5218869b1431f9092418b9e73fb65f5fe
getsometoast <hijameslewis@gmail.com>
2014-11-28 16:42:43 +1100

    add failing test to check the ui tells the simulator to execute the command

--- spec/rea_test/user_interface/menu_spec.rb
+++ spec/rea_test/user_interface/menu_spec.rb
@@ -66,6 +66,30 @@ describe ReaTest::UserInterface::Menu do
       ui_presenter.run
     end
 
+    it 'passes command to the simulator' do
+      fake_stdout = double('$stdout')
+      fake_stdin = double('$stdin')
+      parser = double('Parser')
+      simulator = double('Simulator')
+
+      command = ReaTest::Domain::Command.new type: :report
+
+      expect(fake_stdout).to receive(:puts).twice
+      expect(fake_stdin).to receive(:gets) { "START\n" }
+      expect(fake_stdout).to receive(:puts).twice.with('Enter Command: ')
+      expect(fake_stdin).to receive(:gets) { "REPORT\n" }
+      expect(parser).to receive(:parse).with('REPORT') { command }
+      expect(simulator).to receive(:execute).with(command)
+      expect(fake_stdin).to receive(:gets) { "EXIT\n" }
+
+      ui_presenter = ReaTest::UserInterface::Menu.new stdout: fake_stdout,
+                                                      stdin: fake_stdin,
+                                                      parser: parser,
+                                                      simulator: simulator
+
+      ui_presenter.run
+    end
+
   end
 
 end

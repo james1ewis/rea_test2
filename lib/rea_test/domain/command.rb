b9bca238ab894370514dbb99d57c36b669fbd171
getsometoast <hijameslewis@gmail.com>
2014-11-28 16:39:40 +1100

    add code to make test pass

--- lib/rea_test/domain/command.rb
+++ lib/rea_test/domain/command.rb
@@ -3,6 +3,11 @@ module ReaTest
 
     class Command
       attr_reader :type, :parameters
+
+      def initialize(**args)
+        @type = args.fetch(:type)
+        @parameters = args.fetch(:parameters, nil)
+      end
     end
 
   end
--- lib/rea_test/user_interface/parser.rb
+++ lib/rea_test/user_interface/parser.rb
@@ -3,7 +3,7 @@ module ReaTest
 
     class Parser
       def parse(text)
-
+        return ReaTest::Domain::Command.new type: :report if text == 'REPORT'
       end
     end
 

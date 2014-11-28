fb8dc9126db3ba3005c002008114cc3a3eca4f4d
getsometoast <hijameslewis@gmail.com>
2014-11-28 21:52:31 +1100

    add failing menu test to check it passes move command onto simulator

--- spec/rea_test/user_interface/command_loop_spec.rb
+++ spec/rea_test/user_interface/command_loop_spec.rb
@@ -4,7 +4,7 @@ describe ReaTest::UserInterface::CommandLoop do
 
   describe '#start' do
 
-    it 'passes command to the simulator' do
+    it 'passes report to the simulator' do
       fake_stdout = double('$stdout')
       fake_stdin = double('$stdin')
       parser = double('Parser')
@@ -14,6 +14,47 @@ describe ReaTest::UserInterface::CommandLoop do
       expect(fake_stdin).to receive(:gets) { "REPORT\n" }
       expect(parser).to receive(:parse).with('REPORT') { ReaTest::UserInterface::Command.new type: :report }
       expect(simulator).to receive(:report)
+      expect(fake_stdin).to receive(:gets) { "EXIT\n" }
+
+      command_loop = ReaTest::UserInterface::CommandLoop.new stdout: fake_stdout,
+                                                             stdin: fake_stdin,
+                                                             parser: parser,
+                                                             simulator: simulator
+
+      command_loop.start
+    end
+
+    it 'passes place to the simulator' do
+      fake_stdout = double('$stdout')
+      fake_stdin = double('$stdin')
+      parser = double('Parser')
+      simulator = double('Simulator')
+
+      expect(fake_stdout).to receive(:puts).twice.with('Enter Command: ')
+      expect(fake_stdin).to receive(:gets) { "PLACE 1,2,NORTH\n" }
+      expect(parser).to receive(:parse).with('PLACE 1,2,NORTH') { ReaTest::UserInterface::Command.new type: :place,
+                        parameters: { position: ReaTest::Domain::Position.new(1, 2, :north) }}
+      expect(simulator).to receive(:place)
+      expect(fake_stdin).to receive(:gets) { "EXIT\n" }
+
+      command_loop = ReaTest::UserInterface::CommandLoop.new stdout: fake_stdout,
+                                                             stdin: fake_stdin,
+                                                             parser: parser,
+                                                             simulator: simulator
+
+      command_loop.start
+    end
+
+    it 'passes move to the simulator' do
+      fake_stdout = double('$stdout')
+      fake_stdin = double('$stdin')
+      parser = double('Parser')
+      simulator = double('Simulator')
+
+      expect(fake_stdout).to receive(:puts).twice.with('Enter Command: ')
+      expect(fake_stdin).to receive(:gets) { "MOVE\n" }
+      expect(parser).to receive(:parse).with('MOVE') { ReaTest::UserInterface::Command.new type: :move }
+      expect(simulator).to receive(:move)
       expect(fake_stdin).to receive(:gets) { "EXIT\n" }
 
       command_loop = ReaTest::UserInterface::CommandLoop.new stdout: fake_stdout,

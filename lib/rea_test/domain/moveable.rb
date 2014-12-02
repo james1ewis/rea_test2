ab0773c9902b5b6d71ba9a32fd560278d02b9ce8
getsometoast <hijameslewis@gmail.com>
2014-12-02 18:21:43 +1100

    refactor case statement in moveable#move

--- lib/rea_test/domain/moveable.rb
+++ lib/rea_test/domain/moveable.rb
@@ -3,30 +3,23 @@ module ReaTest
 
     module Moveable
 
+      # this also now doesn't really suit a module... should probably move these all out into classes
       def move
-        new_position = nil
-
-        case self.position.heading
-        when :north then new_position = Position.new(self.position.x, self.position.y + 1, self.position.heading)
-        when :south then new_position = Position.new(self.position.x, self.position.y - 1, self.position.heading)
-        when :east then new_position = Position.new(self.position.x + 1, self.position.y, self.position.heading)
-        when :west then new_position = Position.new(self.position.x - 1, self.position.y, self.position.heading)
-        end
-
+        new_position = get_next_position_from_heading(self.position.heading).call(self.position)
         self.position = new_position unless out_of_bounds? new_position
       end
 
+
+      # it looks like the left and right belong in a class?  Or perhaps rotate should be the only method here...
       def left
         self.position = rotate do |current_heading|
           current_heading == 0 ? current_heading = 3 : current_heading -= 1
-          current_heading
         end
       end
 
       def right
         self.position = rotate do |current_heading|
           current_heading == 3 ? current_heading = 0 : current_heading += 1
-          current_heading
         end
       end
 
@@ -39,6 +32,14 @@ module ReaTest
         current_heading = strategy.call(current_heading)
 
         Position.new(self.position.x, self.position.y, headings[current_heading])
+      end
+
+      def get_next_position_from_heading(heading)
+        { north: Proc.new { |old_position| Position.new(old_position.x, old_position.y + 1, old_position.heading) },
+          south: Proc.new { |old_position| Position.new(old_position.x, old_position.y - 1, old_position.heading) },
+          east: Proc.new { |old_position| Position.new(old_position.x + 1, old_position.y, old_position.heading) },
+          west: Proc.new { |old_position| Position.new(old_position.x - 1, old_position.y, old_position.heading) }
+        }[heading]
       end
 
     end

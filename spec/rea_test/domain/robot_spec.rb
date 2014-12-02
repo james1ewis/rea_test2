26d3e063f0bc0463f17995c34352228a16c49868
getsometoast <hijameslewis@gmail.com>
2014-12-02 15:31:21 +1100

    add more robot#move tests to check all directions don't make robot fall off table

--- spec/rea_test/domain/robot_spec.rb
+++ spec/rea_test/domain/robot_spec.rb
@@ -117,6 +117,51 @@ describe ReaTest::Domain::Robot do
         expect(robot.position.x).to eq(0)
       end
 
+      it 'does not increment robots position heading east' do
+        table = double('Table')
+        initial_position = ReaTest::Domain::Position.new(4, 0, :east)
+        new_position = ReaTest::Domain::Position.new(5, 0, :east)
+
+        expect(table).to receive(:out_of_bounds?).with(initial_position) { false }
+        expect(table).to receive(:out_of_bounds?).with(new_position) { true }
+
+        robot = ReaTest::Domain::Robot.new table: table
+        robot.place(initial_position)
+        robot.move
+
+        expect(robot.position.x).to eq(4)
+      end
+
+      it 'does not increment robots position heading south' do
+        table = double('Table')
+        initial_position = ReaTest::Domain::Position.new(0, 0, :south)
+        new_position = ReaTest::Domain::Position.new(0, -1, :south)
+
+        expect(table).to receive(:out_of_bounds?).with(initial_position) { false }
+        expect(table).to receive(:out_of_bounds?).with(new_position) { true }
+
+        robot = ReaTest::Domain::Robot.new table: table
+        robot.place(initial_position)
+        robot.move
+
+        expect(robot.position.y).to eq(0)
+      end
+
+      it 'does not increment robots position heading north' do
+        table = double('Table')
+        initial_position = ReaTest::Domain::Position.new(4, 4, :north)
+        new_position = ReaTest::Domain::Position.new(4, 5, :north)
+
+        expect(table).to receive(:out_of_bounds?).with(initial_position) { false }
+        expect(table).to receive(:out_of_bounds?).with(new_position) { true }
+
+        robot = ReaTest::Domain::Robot.new table: table
+        robot.place(initial_position)
+        robot.move
+
+        expect(robot.position.y).to eq(4)
+      end
+
     end
 
   end

7ad231a0947c5f3f3fcd6017ac75f8cca177cf4b
getsometoast <hijameslewis@gmail.com>
2014-12-02 16:44:01 +1100

    add all failing tests for robot #right - big steps

--- spec/rea_test/domain/robot_spec.rb
+++ spec/rea_test/domain/robot_spec.rb
@@ -222,4 +222,60 @@ describe ReaTest::Domain::Robot do
 
   end
 
+  describe '#right' do
+
+    it 'rotates the robot right from north' do
+      table = double('Table')
+      initial_position = ReaTest::Domain::Position.new(4, 4, :north)
+
+      allow(table).to receive(:out_of_bounds?) { false }
+
+      robot = ReaTest::Domain::Robot.new table: table
+      robot.place(initial_position)
+      robot.left
+
+      expect(robot.position.heading).to eq(:east)
+    end
+
+    it 'rotates the robot right from west' do
+      table = double('Table')
+      initial_position = ReaTest::Domain::Position.new(4, 4, :west)
+
+      allow(table).to receive(:out_of_bounds?) { false }
+
+      robot = ReaTest::Domain::Robot.new table: table
+      robot.place(initial_position)
+      robot.left
+
+      expect(robot.position.heading).to eq(:north)
+    end
+
+    it 'rotates the robot right from south' do
+      table = double('Table')
+      initial_position = ReaTest::Domain::Position.new(4, 4, :south)
+
+      allow(table).to receive(:out_of_bounds?) { false }
+
+      robot = ReaTest::Domain::Robot.new table: table
+      robot.place(initial_position)
+      robot.left
+
+      expect(robot.position.heading).to eq(:west)
+    end
+
+    it 'rotates the robot right from east' do
+      table = double('Table')
+      initial_position = ReaTest::Domain::Position.new(4, 4, :east)
+
+      allow(table).to receive(:out_of_bounds?) { false }
+
+      robot = ReaTest::Domain::Robot.new table: table
+      robot.place(initial_position)
+      robot.left
+
+      expect(robot.position.heading).to eq(:south)
+    end
+
+  end
+
 end

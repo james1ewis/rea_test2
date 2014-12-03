e5ec73b1ebbc1d797f0fbbab29a445afae57131f
getsometoast <hijameslewis@gmail.com>
2014-12-03 12:31:27 +1100

    remove table dependecy from placer - might be able to remove this class now

--- lib/rea_test/domain/placer.rb
+++ lib/rea_test/domain/placer.rb
@@ -2,12 +2,8 @@ module ReaTest
   module Domain
 
     class Placer
-      def initialize(**args)
-        @table = args.fetch(:table, Table.new)
-      end
-
       def place(placeable, position)
-        placeable.position = position unless @table.out_of_bounds? position
+        placeable.position = position
       end
     end
 
--- spec/rea_test/domain/placer_spec.rb
+++ spec/rea_test/domain/placer_spec.rb
@@ -4,35 +4,14 @@ describe ReaTest::Domain::Placer do
 
   describe '#place' do
 
-    context 'the position is within the table bounds' do
+    it 'update the robots current position' do
+      robot = double('Robot')
+      position = ReaTest::Domain::Position.new(1, 2, :north)
+      placer = ReaTest::Domain::Placer.new
 
-      it 'update the robots current position' do
-        table = double('Table')
-        robot = double('Robot')
-        position = ReaTest::Domain::Position.new(1, 2, :north)
-        placer = ReaTest::Domain::Placer.new table: table
+      expect(robot).to receive(:position=).with(position)
 
-        expect(table).to receive(:out_of_bounds?).with(position) { false }
-        expect(robot).to receive(:position=).with(position)
-
-        placer.place(robot, position)
-      end
-
-    end
-
-    context 'the position is out of the table bounds' do
-
-      it 'update the robots current position' do
-        table = double('Table')
-        robot = double('Robot')
-        position = ReaTest::Domain::Position.new(-11, 2, :north)
-        placer = ReaTest::Domain::Placer.new table: table
-
-        expect(table).to receive(:out_of_bounds?).with(position) { true }
-
-        placer.place(robot, position)
-      end
-
+      placer.place(robot, position)
     end
 
   end

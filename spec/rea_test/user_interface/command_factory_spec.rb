359067f1f943d18730f1dbe1d8da1e30beac456d
getsometoast <hijameslewis@gmail.com>
2014-12-03 21:15:30 +1100

    add failing tests for other commands

--- spec/rea_test/user_interface/command_factory_spec.rb
+++ spec/rea_test/user_interface/command_factory_spec.rb
@@ -13,6 +13,42 @@ describe ReaTest::UserInterface::CommandFactory do
       expect(command).to be_a(ReaTest::UserInterface::PlaceCommand)
     end
 
+    it 'creates a report command' do
+      simulator = double('Simulator')
+      command_factory = ReaTest::UserInterface::CommandFactory.new simulator: simulator
+
+      command = command_factory.create(:report)
+
+      expect(command).to be_a(ReaTest::UserInterface::ReportCommand)
+    end
+
+    it 'creates a move command' do
+      simulator = double('Simulator')
+      command_factory = ReaTest::UserInterface::CommandFactory.new simulator: simulator
+
+      command = command_factory.create(:move)
+
+      expect(command).to be_a(ReaTest::UserInterface::MoveCommand)
+    end
+
+    it 'creates a left command' do
+      simulator = double('Simulator')
+      command_factory = ReaTest::UserInterface::CommandFactory.new simulator: simulator
+
+      command = command_factory.create(:left)
+
+      expect(command).to be_a(ReaTest::UserInterface::LeftCommand)
+    end
+
+    it 'creates a right command' do
+      simulator = double('Simulator')
+      command_factory = ReaTest::UserInterface::CommandFactory.new simulator: simulator
+
+      command = command_factory.create(:right)
+
+      expect(command).to be_a(ReaTest::UserInterface::RightCommand)
+    end
+
   end
 
 end

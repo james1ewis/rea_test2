adcb8e9e0f3c5205472ee646c67935523207a3fa
getsometoast <hijameslewis@gmail.com>
2014-12-03 21:20:05 +1100

    add the simulator to the commands as a collaborator

--- lib/rea_test/user_interface/command_factory.rb
+++ lib/rea_test/user_interface/command_factory.rb
@@ -8,11 +8,11 @@ module ReaTest
 
       def create(type, **parameters)
         case type
-        when :place then PlaceCommand.new position: parameters[:position]
-        when :report then ReportCommand.new
-        when :move then MoveCommand.new
-        when :left then LeftCommand.new
-        when :right then RightCommand.new
+        when :place then PlaceCommand.new simulator: @simulator, position: parameters[:position]
+        when :report then ReportCommand.new simulator: @simulator
+        when :move then MoveCommand.new simulator: @simulator
+        when :left then LeftCommand.new simulator: @simulator
+        when :right then RightCommand.new simulator: @simulator
         end
       end
     end

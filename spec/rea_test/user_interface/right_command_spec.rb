a2bff42ab3a56301a89839d86dd27ddc4e08f05a
getsometoast <hijameslewis@gmail.com>
2014-12-03 21:29:24 +1100

    add failing test for rightcommand#execute

---
+++ spec/rea_test/user_interface/right_command_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../../support/spec_helper'
+
+describe ReaTest::UserInterface::RightCommand do
+
+  describe '#execute' do
+
+    it 'calls right on the simulator' do
+      simulator = double('Simulator')
+
+      expect(simulator).to receive(:right)
+
+      right = ReaTest::UserInterface::RightCommand.new simulator: simulator
+      right.execute
+    end
+
+  end
+
+end

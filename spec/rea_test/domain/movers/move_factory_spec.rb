4988c55b8f68b4515d76e0ed4145a6596a434709
getsometoast <hijameslewis@gmail.com>
2014-12-04 08:56:57 +1100

    remove duplication from mover factory spec

--- spec/rea_test/domain/movers/move_factory_spec.rb
+++ spec/rea_test/domain/movers/move_factory_spec.rb
@@ -4,31 +4,27 @@ describe ReaTest::Domain::Movers::MoveFactory do
 
   describe '#create' do
 
-    it 'creates a north_mover' do
-      factory = ReaTest::Domain::Movers::MoveFactory.new
-      mover = factory.create(:north)
+    before :each do
+      @factory = ReaTest::Domain::Movers::MoveFactory.new
+    end
 
+    it 'creates a north_mover' do
+      mover = @factory.create(:north)
       expect(mover).to be_a(ReaTest::Domain::Movers::NorthMover)
     end
 
     it 'creates a south_mover' do
-      factory = ReaTest::Domain::Movers::MoveFactory.new
-      mover = factory.create(:south)
-
+      mover = @factory.create(:south)
       expect(mover).to be_a(ReaTest::Domain::Movers::SouthMover)
     end
 
     it 'creates a east_mover' do
-      factory = ReaTest::Domain::Movers::MoveFactory.new
-      mover = factory.create(:east)
-
+      mover = @factory.create(:east)
       expect(mover).to be_a(ReaTest::Domain::Movers::EastMover)
     end
 
     it 'creates a west_mover' do
-      factory = ReaTest::Domain::Movers::MoveFactory.new
-      mover = factory.create(:west)
-
+      mover = @factory.create(:west)
       expect(mover).to be_a(ReaTest::Domain::Movers::WestMover)
     end
 

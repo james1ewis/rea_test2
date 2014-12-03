db7322ff7dd905568f9750ef51beec87cf834fd2
getsometoast <hijameslewis@gmail.com>
2014-12-03 23:03:14 +1100

    make rotater get_next_heading method protected

--- lib/rea_test/domain/right_rotater.rb
+++ lib/rea_test/domain/right_rotater.rb
@@ -4,6 +4,9 @@ module ReaTest
   module Domain
 
     class RightRotater < Rotater
+
+      protected
+
       def get_next_heading(current_heading)
         current_heading == 3 ? current_heading = 0 : current_heading += 1
       end

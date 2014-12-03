d8b863eed26c1483e8df144d8ac5f6c2011471a5
getsometoast <hijameslewis@gmail.com>
2014-12-03 22:43:50 +1100

    remove duplication in command_factory spec

--- spec/rea_test/user_interface/commands/command_factory_spec.rb
+++ spec/rea_test/user_interface/commands/command_factory_spec.rb
@@ -4,48 +4,33 @@ describe ReaTest::UserInterface::Commands::CommandFactory do
 
   describe '#create' do
 
-    it 'creates a place command' do
+    before :each do
       simulator = double('Simulator')
-      command_factory = ReaTest::UserInterface::Commands::CommandFactory.new simulator: simulator
+      @command_factory = ReaTest::UserInterface::Commands::CommandFactory.new simulator: simulator
+    end
 
-      command = command_factory.create(:place, { position: ReaTest::Domain::Position.new(1, 2, :north) })
-
+    it 'creates a place command' do
+      command = @command_factory.create(:place, { position: ReaTest::Domain::Position.new(1, 2, :north) })
       expect(command).to be_a(ReaTest::UserInterface::Commands::PlaceCommand)
     end
 
     it 'creates a report command' do
-      simulator = double('Simulator')
-      command_factory = ReaTest::UserInterface::Commands::CommandFactory.new simulator: simulator
-
-      command = command_factory.create(:report)
-
+      command = @command_factory.create(:report)
       expect(command).to be_a(ReaTest::UserInterface::Commands::ReportCommand)
     end
 
     it 'creates a move command' do
-      simulator = double('Simulator')
-      command_factory = ReaTest::UserInterface::Commands::CommandFactory.new simulator: simulator
-
-      command = command_factory.create(:move)
-
+      command = @command_factory.create(:move)
       expect(command).to be_a(ReaTest::UserInterface::Commands::MoveCommand)
     end
 
     it 'creates a left command' do
-      simulator = double('Simulator')
-      command_factory = ReaTest::UserInterface::Commands::CommandFactory.new simulator: simulator
-
-      command = command_factory.create(:left)
-
+      command = @command_factory.create(:left)
       expect(command).to be_a(ReaTest::UserInterface::Commands::LeftCommand)
     end
 
     it 'creates a right command' do
-      simulator = double('Simulator')
-      command_factory = ReaTest::UserInterface::Commands::CommandFactory.new simulator: simulator
-
-      command = command_factory.create(:right)
-
+      command = @command_factory.create(:right)
       expect(command).to be_a(ReaTest::UserInterface::Commands::RightCommand)
     end
 

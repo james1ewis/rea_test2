cc96d4230b5e5fa42d95ac02303b0ba11ea5c7d9
getsometoast <hijameslewis@gmail.com>
2014-12-03 18:32:23 +1100

    add failing test for ReportCommand#execute

---
+++ lib/rea_test/user_interface/report_comand.rb
@@ -0,0 +1,8 @@
+module ReaTest
+  module UserInterface
+
+    class ReportComand
+    end
+    
+  end
+end
---
+++ spec/rea_test/user_interface/report_command_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../support/spec_helper'
+
+describe ReaTest::UserInterface::ReportCommand do
+
+  describe '#execute' do
+
+    it 'reports the robots position' do
+
+    end
+
+  end
+
+end

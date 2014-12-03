dee67cb091193a214b73b6f3001068961f911a6d
getsometoast <hijameslewis@gmail.com>
2014-12-03 12:34:17 +1100

    remove placer

--- spec/support/spec_helper.rb
+++ spec/support/spec_helper.rb
@@ -7,7 +7,6 @@ require_relative '../../lib/rea_test/domain/position'
 require_relative '../../lib/rea_test/domain/Robot'
 require_relative '../../lib/rea_test/domain/simulator'
 require_relative '../../lib/rea_test/domain/table'
-require_relative '../../lib/rea_test/domain/placer'
 require_relative '../../lib/rea_test/domain/mover'
 require_relative '../../lib/rea_test/domain/rotater'
 

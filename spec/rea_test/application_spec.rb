ab15aee3027f131f350f267d06220a8d9eca6cc6
getsometoast <hijameslewis@gmail.com>
2014-12-03 21:39:15 +1100

    add failing test in application to wire up the menu to the non-existent observable simulator

--- spec/rea_test/application_spec.rb
+++ spec/rea_test/application_spec.rb
@@ -4,12 +4,23 @@ describe ReaTest::Application do
 
   describe '#run' do
 
-    it 'instantiates the user interface' do
+    it 'instantiates the main menu' do
       ui_presenter = double('UIPresenter')
+
       expect(ui_presenter).to receive(:run)
 
       application = ReaTest::Application.new ui_presenter: ui_presenter
+      application.run
+    end
 
+    it 'adds the main menu as a listener to the observable simulator' do
+      ui_presenter = double('UIPresenter')
+      simulator = double('ObservableSimulator')
+
+      expect(ui_presenter).to receive(:run)
+      expect(simulator).to receive(:add_listener).with(ui_presenter)
+
+      application = ReaTest::Application.new ui_presenter: ui_presenter
       application.run
     end
 
